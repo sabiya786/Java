4131d8e8a9e54e2b2e605bfb78eae4a9491261a6
Sabiya <shaikhsabiya5@gmail.com>
2022-12-31 10:34:59 +0530

    oops program

---
+++ JavaProgram/src/oopsProgram/EncapsulationProgram1.java
@@ -0,0 +1,17 @@
+package oopsProgram;
+
+public class EncapsulationProgram1 
+{
+	private int a;
+
+	public int geta()
+	{
+		return a;
+	}
+
+	public void seta(int a)
+	{
+		this.a=a;
+	}
+
+}
---
+++ JavaProgram/src/oopsProgram/EncapsulationProgram2.java
@@ -0,0 +1,13 @@
+package oopsProgram;
+
+public class EncapsulationProgram2 
+{
+	public static void main(String []args)
+	{
+		EncapsulationProgram1 obj = new EncapsulationProgram1();
+		obj.seta(10);
+		int b = obj.geta();
+		System.out.println(b);
+	}
+
+}
